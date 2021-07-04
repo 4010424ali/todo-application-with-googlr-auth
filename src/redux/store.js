435cc1406946ddc318cb3c984cbbb841a255a25f
Ali Hamza <ali.hamza4010424@gmail.com>
2021-07-04 20:03:56 +0500

    some fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -21,7 +21,9 @@ const store = createStore(
   initailState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
   )
 );
 
